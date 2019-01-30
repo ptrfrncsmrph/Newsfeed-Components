ed240be4c93f8fba1a67cb89f153ea2242e99b7c
Peter Murphy <peter.francis.murphy@gmail.com>
2019-01-30 14:45:55 -0500

    Removes method arrow function, replaces with explicit bind

--- Article/Article.js
+++ Article/Article.js
@@ -9,13 +9,13 @@ class Article {
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.innerText = "expand"
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton.addEventListener("click", this.expandArticle)
+    this.expandButton.addEventListener("click", this.expandArticle.bind(this))
   }
   static of(domElement) {
     return new Article(domElement)
   }
   // This works in Chrome, but not in Firefox :-(
-  expandArticle = () => {
+  expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     console.log(this.domElement)
   }

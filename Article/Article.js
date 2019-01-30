af9f9896b32cd3d1c90886ab97accfab3a9d6263
Peter Murphy <peter.francis.murphy@gmail.com>
2019-01-30 14:55:44 -0500

    Adds window.onload listener, removes comments

--- Article/Article.js
+++ Article/Article.js
@@ -1,33 +1,19 @@
-// Because classes are not hoised you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
-
 class Article {
   constructor(domElement) {
-    // assign this.domElement to the passed in domElement
     this.domElement = domElement
-    // create a reference to the ".expandButton" class.
     this.expandButton = domElement.querySelector(".expandButton")
-    // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.innerText = "expand"
-    // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener("click", this.expandArticle.bind(this))
   }
   static of(domElement) {
     return new Article(domElement)
   }
-  // This works in Chrome, but not in Firefox :-(
   expandArticle() {
-    // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.domElement.classList.toggle("article-open")
   }
 }
 
-/* START HERE: 
-
-- Select all classes named ".article" and assign that value to the articles variable.  
-
-- With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the constructor.
-
-*/
-
-let articles = document.querySelectorAll(".article")
-articles.forEach(Article.of)
+window.onload = () => {
+  let articles = document.querySelectorAll(".article")
+  articles.forEach(Article.of)
+}

45163f8f7f2879b7cae3ffa0deb0df7f7d49a44c
Peter Murphy <peter.francis.murphy@gmail.com>
2019-01-30 17:44:14 -0500

    Cleans up Article.js

--- Article/Article.js
+++ Article/Article.js
@@ -1,25 +1,23 @@
 class Article {
   constructor(domElement) {
     this.domElement = domElement
-    this.expandArticle()
+    this.domElement.classList.add("article-open")
     this.height = getComputedStyle(domElement).height
-    this.expandArticle()
+    this.domElement.classList.remove("article-open")
     this.expandButton = domElement.querySelector(".expandButton")
     this.expandButton.innerText = "expand"
-    this.expandButton.addEventListener("click", this.$expandArticle.bind(this))
+    this.expandButton.addEventListener("click", this.expandArticle.bind(this))
     this.isExpanded = false
   }
   static of(domElement) {
     return new Article(domElement)
   }
   expandArticle() {
-    this.domElement.classList.toggle("article-open")
-  }
-  $expandArticle() {
     this.isExpanded
       ? (this.domElement.style.height = "50px")
       : (this.domElement.style.height = this.height)
     this.isExpanded = !this.isExpanded
+    this.expandButton.innerText = this.isExpanded ? "collapse" : "expand"
   }
 }
 

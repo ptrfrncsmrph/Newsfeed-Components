08df5efa7d37e542c56ab4bd22dc80304a925927
Peter Murphy <peter.francis.murphy@gmail.com>
2019-01-30 15:05:52 -0500

    Expand button expands height to a dynamic value (based on length of article)

--- Article/Article.js
+++ Article/Article.js
@@ -1,15 +1,25 @@
 class Article {
   constructor(domElement) {
     this.domElement = domElement
+    this.expandArticle()
+    this.height = getComputedStyle(domElement).height
+    this.expandArticle()
     this.expandButton = domElement.querySelector(".expandButton")
     this.expandButton.innerText = "expand"
-    this.expandButton.addEventListener("click", this.expandArticle.bind(this))
+    this.expandButton.addEventListener("click", this.$expandArticle.bind(this))
+    this.isExpanded = false
   }
   static of(domElement) {
     return new Article(domElement)
   }
   expandArticle() {
     this.domElement.classList.toggle("article-open")
+  }
+  $expandArticle() {
+    this.isExpanded
+      ? (this.domElement.style.height = "50px")
+      : (this.domElement.style.height = this.height)
+    this.isExpanded = !this.isExpanded
   }
 }
 

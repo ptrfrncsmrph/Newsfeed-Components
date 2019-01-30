e0741cf11191f12beec25b83d9747b7f6ddf014f
Peter Murphy <peter.francis.murphy@gmail.com>
2019-01-30 14:45:02 -0500

    Can't use arrow function on class methods
    In Firefox apparently, in Chrome this still works

--- Article/Article.js
+++ Article/Article.js
@@ -9,13 +9,15 @@ class Article {
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.innerText = "expand"
     // Set a click handler on the expandButton reference, calling the expandArticle method.
+    this.expandButton.addEventListener("click", this.expandArticle)
   }
   static of(domElement) {
     return new Article(domElement)
   }
-
-  expandArticle() {
+  // This works in Chrome, but not in Firefox :-(
+  expandArticle = () => {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
+    console.log(this.domElement)
   }
 }
 
